92147a904f6fee234110d02fd4c7e4ffe7970774
Alexis de Lattre <alexis.delattre@akretion.com>
2026-09-18 23:52:21 +0200

    [IMP] account_tax_unece: improve VATEX support and UNECE checks
    Add python constrain to check UNECE config
    Set field unece_categ_id as required on sale VAT taxes

--- account_tax_unece/models/account_tax.py
+++ account_tax_unece/models/account_tax.py
@@ -3,6 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AccountTax(models.Model):
@@ -62,7 +63,7 @@ class AccountTax(models.Model):
     # So we don't need the field unece_due_date_id any more.
     # We replace it by _get_unece_due_date_type_code() below.
 
-    @api.depends("unece_categ_id", "unece_type_id", "type_tax_use")
+    @api.depends("type_tax_use", "unece_type_id", "unece_categ_id")
     def _compute_unece_vatex_id(self):
         automap = {
             "K": self.env.ref(
@@ -70,6 +71,9 @@ class AccountTax(models.Model):
             ),
             "G": self.env.ref(
                 "account_tax_unece.tax_vatex_eu_g", raise_if_not_found=False
+            ),
+            "O": self.env.ref(
+                "account_tax_unece.tax_vatex_eu_o", raise_if_not_found=False
             ),
         }
         for tax in self:
@@ -87,6 +91,46 @@ class AccountTax(models.Model):
                 or tax.type_tax_use != "sale"
             ):
                 tax.unece_vatex_id = False
+
+    @api.constrains(
+        "type_tax_use",
+        "active",
+        "unece_type_id",
+        "unece_categ_id",
+        "unece_vatex_id",
+    )
+    def _check_unece_config(self):
+        for tax in self:
+            if tax.unece_type_id and tax.unece_type_id.code == "VAT":
+                if tax.amount_type != "percent":
+                    raise ValidationError(
+                        self.env._(
+                            "Tax '%s' has UNECE Tax Category 'VAT' so its "
+                            "Tax Computation should be 'Percentage'.",
+                            tax.display_name,
+                        )
+                    )
+                if tax.type_tax_use == "sale" and tax.active:
+                    if not tax.unece_categ_id:
+                        raise ValidationError(
+                            self.env._(
+                                "On VAT tax '%s' you must set an UNECE Tax Category.",
+                                tax.display_name,
+                            )
+                        )
+                    if (
+                        tax.unece_categ_id.code in ("E", "G", "K", "O", "AE")
+                        and not tax.unece_vatex_id
+                    ):
+                        raise ValidationError(
+                            self.env._(
+                                "On VAT tax '%(tax)s' that has UNECE Category "
+                                "'%(unece_categ)s', you must set a "
+                                "VAT Exemption Reason Code.",
+                                tax=tax.display_name,
+                                unece_categ=tax.unece_categ_id.display_name,
+                            )
+                        )
 
     @api.model
     def _get_unece_code_from_tax_exigibility(self, tax_exigibility):
